fb71ce9c539864cd707ae13a728dec1b76eb8bb0
Carlos Manoel <carlosmanoeldev@outlook.com>
2026-07-03 18:52:22 -0300

    Initial commit

---
+++ src/main/java/br/com/CarlosManoel/Gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,19 @@
+package br.com.CarlosManoel.Gestao_vagas.modules.candidate;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
+
+
+}
