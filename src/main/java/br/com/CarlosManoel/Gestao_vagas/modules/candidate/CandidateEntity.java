a7986bf7583827b04d2395890302ec7a722112d1
Carlos Manoel <carlosmanoeldev@outlook.com>
2026-07-04 20:42:29 -0300

    Correção da validação de espaços <username>

--- src/main/java/br/com/CarlosManoel/Gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/CarlosManoel/Gestao_vagas/modules/candidate/CandidateEntity.java
@@ -12,13 +12,13 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
-    @Pattern(regexp = "^(?!\\s*$).+", message = "O username não deve conter espaços")
+    @Pattern(regexp = "\\S+", message = "O username não deve conter espaços")
     private String username;
 
     @Email( message = "O email deve ser válido")
     private String email;
 
-    @Length(min = 8, max = 50)
+    @Length(min = 8, max = 50, message = "Senha deve Ser maior que 8 caracteres")
     private String password;
     private String description;
     private String curriculum;
