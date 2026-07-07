65e1c63a9be9ddc51d8ac4f8a3d4efab7ec5cb58
Carlos Manoel <carlosmanoeldev@outlook.com>
2026-07-07 15:43:17 -0300

    Validação usuário duplicado

---
+++ src/main/java/br/com/CarlosManoel/Gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -0,0 +1,26 @@
+package br.com.CarlosManoel.Gestao_vagas.modules.candidate.useCases;
+
+import br.com.CarlosManoel.Gestao_vagas.exceptions.UserFoundException;
+import br.com.CarlosManoel.Gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.CarlosManoel.Gestao_vagas.modules.candidate.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public CandidateEntity execute(CandidateEntity candidateEntity){
+        this.candidateRepository.findByUsernameOrEmail
+                        (candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+
+
+        return this.candidateRepository.save(candidateEntity);
+
+    }
+}
